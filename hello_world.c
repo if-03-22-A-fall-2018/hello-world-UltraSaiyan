bdace90dc974e302267d7c30cb8dfe9c931bfa0a
Adam Altalbani <adam.altalbani@hotmail.com>
2018-09-18 11:29:55 +0200

    Edited Block Header Comment

--- hello_world.c
+++ hello_world.c
@@ -1,3 +1,16 @@
+
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 2AHIF
+ * ---------------------------------------------------------
+ * Exercise Number: 0
+ * Title:  hello world
+ * Author: Adam Altalbani
+ * ----------------------------------------------------------
+ * Description:
+ * The Program shows "Hello" plus the name of the Author.
+ * ----------------------------------------------------------
+*/
+
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
